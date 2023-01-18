1fb0fc325f76de325885d77f8f00e08c571afec4
Thomas Augot <thomas.augot@hotmail.fr>
2023-01-18 15:45:18 +0100

    adding a search functionnality

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ function App() {
 
   const [moviesArr, setMoviesArr] = useState(moviesFromJson);
 
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const moviesToDisplay = moviesArr.filter( (movie) => {
+    return movie.title.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
+
 
   const deleteMovie = (movieTitle) => {
 
@@ -38,9 +45,25 @@ function App() {
 
   return (
     <div className="App">
-      <Header numberOfMovies={moviesArr.length} />
+      <Header numberOfMovies={moviesToDisplay.length} />
       <AddMovie callbackToCreate={createMovie} />
-      <Main moviesArr={moviesArr} callbackToDelete={deleteMovie} />
+
+      <hr />
+      <form>
+        <label>
+            Search by Title:
+            <input 
+              type="text" 
+              name="searchQuery" 
+              placeholder="search by title" 
+              value={searchQuery}
+              onChange={(e) => { setSearchQuery(e.target.value) }} 
+              />
+        </label>
+      </form>
+      <hr />
+
+      <Main moviesArr={moviesToDisplay} callbackToDelete={deleteMovie} />
       <Footer />
     </div>
   );
